ac753f44bc75df128f98a54e64fa05567209c16d
Volodymyr Protskyi <volodimir.protskiy@gmail.com>
2026-01-22 17:41:22 +0200

    provide get me logic in auth service

--- webApi/src/features/auth/auth.service.ts
+++ webApi/src/features/auth/auth.service.ts
@@ -9,11 +9,13 @@ import * as bcrypt from 'bcrypt';
 
 import { UsersService } from '../users/users.service';
 
-import { generateTokens } from './utils/token.utils';
-
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
+import { UserResponseDto } from '../users/dto/user-response.dto';
+import { type JwtPayload } from './interfaces/jwt-payload.interface';
 
+import { generateTokens } from './utils/token.utils';
+import { CurrentUser } from 'src/common/decorators/current-user-decorator';
 import { ENCRYPTION_SALT } from 'src/common/constants/auth.constants';
 
 @Injectable()
@@ -78,5 +80,16 @@ export class AuthService {
     await this.usersService.updateRefreshToken(user.id, hashedRefreshToken);
 
     return tokens;
+  }
+
+  async getMe(@CurrentUser() user: JwtPayload): Promise<UserResponseDto> {
+    const foundUser = await this.usersService.findById(user.userId);
+
+    if (!foundUser) {
+      throw new UnauthorizedException('User is not found');
+    }
+
+    const { id, email, firstName, lastName, avatarUrl } = foundUser;
+    return { id, email, firstName, lastName, avatarUrl };
   }
 }
