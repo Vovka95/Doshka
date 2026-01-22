5a5bcae4379bc76c15b33ced4c5dedc7c1fe57ff
Volodymyr Protskyi <volodimir.protskiy@gmail.com>
2026-01-22 17:42:05 +0200

    provide auth routes

---
+++ webApi/src/features/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+import { SignupDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import { type JwtPayload } from './interfaces/jwt-payload.interface';
+
+import { CurrentUser } from 'src/common/decorators/current-user-decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signup(@Body() dto: SignupDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  async login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async me(@CurrentUser() user: JwtPayload) {
+    return this.authService.getMe(user);
+  }
+}
