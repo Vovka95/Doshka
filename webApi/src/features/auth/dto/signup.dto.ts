4dfc0beb3785aeaf3dfa67038cec7912e19d301c
Volodymyr Protskyi <volodimir.protskiy@gmail.com>
2026-01-22 17:04:06 +0200

    create Match validator decorator

--- webApi/src/features/auth/dto/signup.dto.ts
+++ webApi/src/features/auth/dto/signup.dto.ts
@@ -4,6 +4,7 @@ import {
   PASSWORD_MAX_LENGTH,
   PASSWORD_MIN_LENGTH,
 } from 'src/common/constants/auth.constants';
+import { Match } from 'src/common/validators/match.validator';
 
 export class SignupDto {
   @IsNotEmpty()
@@ -19,6 +20,7 @@ export class SignupDto {
   @MaxLength(PASSWORD_MAX_LENGTH)
   password: string;
 
+  @Match('password', { message: 'Passwords do not match' })
   @MinLength(PASSWORD_MIN_LENGTH)
   @MaxLength(PASSWORD_MAX_LENGTH)
   confirmPassword: string;
