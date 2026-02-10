df47b9dece108697ec0188f4ddb49f51600c5b8d
Volodymyr Protskyi <volodimir.protskiy@gmail.com>
2026-02-10 12:38:30 +0200

    provide app structure

--- webClient/src/main.tsx
+++ webClient/src/main.tsx
@@ -1,18 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import App from "./App.tsx";
-
-import "./index.css";
-
-const queryClient = new QueryClient();
+import { AppProviders } from "@/app/providers/AppProviders";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
-    </StrictMode>
+        <AppProviders>
+            <></>
+        </AppProviders>
+    </StrictMode>,
 );
