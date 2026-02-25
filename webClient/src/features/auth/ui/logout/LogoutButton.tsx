b96003f598cd5856e4aa2b729e9f42305d204432
Volodymyr Protskyi <volodimir.protskiy@gmail.com>
2026-02-25 12:11:22 +0200

    fix logout bug

--- webClient/src/features/auth/ui/logout/LogoutButton.tsx
+++ webClient/src/features/auth/ui/logout/LogoutButton.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 
 import { Button } from "@/shared/ui";
@@ -8,12 +7,10 @@ import { authSession } from "../../lib/authSession";
 import { useLogoutMutation } from "../../model";
 
 import { useUIStore } from "@/shared/store/ui";
-import { routes } from "@/app/config/routes";
 import { normalizeApiError } from "@/shared/api/http/errror";
 
 export const LogoutButton = () => {
     const queryClient = useQueryClient();
-    const navigate = useNavigate();
     const toast = useUIStore((s) => s.toast);
     const logoutMutation = useLogoutMutation();
 
@@ -28,8 +25,6 @@ export const LogoutButton = () => {
                 title: "Signed out",
                 message: "You've been logged out.",
             });
-
-            navigate(routes.login(), { replace: true });
         } catch (error) {
             authSession.clear(queryClient);
 
@@ -40,8 +35,6 @@ export const LogoutButton = () => {
                 title: "Logout failed",
                 message: apiError.message,
             });
-
-            navigate(routes.login(), { replace: true });
         }
     };
 
